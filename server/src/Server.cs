f7448018806adf3b19f99e9da502edd663f7c8cc
Nicolas Antonio Cloutier <nicocloutier1@gmail.com>
2024-09-10 16:27:07 -0500

    develop server boilerplate

---
+++ server/src/Server.cs
@@ -0,0 +1,10 @@
+﻿namespace src;
+
+public class Server {
+    public Server() {
+        var con = new NpgsqlConnection(connectionString: "";)
+        con.Open();
+        using var cmd = new NpgsqlCommand();
+        cmd.Connection = con;
+    }
+}
