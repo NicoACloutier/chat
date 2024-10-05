72132bc64ef18fe0d9ad95e50a92bf78ce48daf9
Nicolas Antonio Cloutier <nicocloutier1@gmail.com>
2024-10-04 20:56:14 -0500

    write message endpoint functions

--- server/routes/messages.py
+++ server/routes/messages.py
@@ -1,14 +1,32 @@
 import uuid, psycopg2, flask
 from flask import request
+from psycopg2 import ProgrammingError
 
 def get_messages_from_info(conn, chat_id: str) -> tuple[dict[str, str], int]:
-    pass
+    with conn.cursor() as cursor:
+        cursor.execute(f"SELECT * FROM messages WHERE chat_id = '{chat_id}'")
+        try:
+            info = cursor.fetchall()
+        except ProgrammingError:
+            return {}, 404
+    return info, 200
 
 def delete_old_message(conn, message_id: str) -> tuple[dict[str, str], int]:
-    pass
+    with conn.cursor() as cursor:
+        cursor.execute(f"DELETE * FROM messages WHERE id = '{message_id}' RETURNING *;")
+        try:
+            info = cursor.fetchone()
+        except ProgrammingError:
+            return {}, 404
+    return info, 200
 
 def post_new_message(conn) -> tuple[dict[str, str], int]:
-    pass
+    data = flask.request.get_json()
+    message, chat_id, user_id = data["message"], data["chat_id"], data["user_id"]
+    message_id = uuid.uuid4()
+    with conn.cursor() as cursor:
+        cursor.execute(f"INSERT INTO messages (id, message, chat_id, user_id) VALUES ('{message_id}', '{message}', '{chat_id}', '{user_id}');")
+    return {"message": "User added."}, 200
 
 def create_message_api(app: flask.Flask, conn) -> None:
     ENDPOINT_NAME = "/messages"
